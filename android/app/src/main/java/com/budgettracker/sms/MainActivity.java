d435046d8c9ba64c8309313f9982f3f01b24a643
Nishi Mayekar <65915858+Nishi-Mayekar@users.noreply.github.com>
2026-04-17 21:19:26 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/budgettracker/sms/MainActivity.java
+++ android/app/src/main/java/com/budgettracker/sms/MainActivity.java
@@ -1,5 +1,10 @@
 package com.budgettracker.sms;
-
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(SmsPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
